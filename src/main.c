43b700d6201ccc1540d6e6ae509a1a10c38133fa
Ryan <murphyslaw480@gmail.com>
2013-10-19 13:14:20 -0400

    Add constants from python project in header

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include "hello.h"
+#include "common.h"
 
 int main(int argc, char *argv[]) {
     say_hello();
+    printf("\n");
+    printf(FILE_NAME);
+    printf("\n");
     return 0;
 }
