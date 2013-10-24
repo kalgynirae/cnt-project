baf11f660e76948b69d1544c24899f23f45c0a2a
Ryan <murphyslaw480@gmail.com>
2013-10-24 15:35:25 -0400

    parse PeerInfo.cfg

--- lib/init.h
+++ lib/init.h
@@ -16,8 +16,24 @@ struct common_cfg
     int piece_size;
 };
 
+struct peer_info
+{
+    int peer_id;        //unique identifier of peer
+    char* hostname;     //host name
+    int port;           //port on which peer listens
+    int has_file;       //1 if peer has complete file, else 0
+};
+
 //read config file and return struct containing options
 struct common_cfg read_cfg(char* cfg_file_name);
-//read config file and return struct containing options
+
+/* read config file and assign array of peer_info structs to result
+ * returns number of peers
+ */
+struct peer_info* read_peers(char* cfg_file_name, int* num_peers);
+
+//parse a line of the common config file
 void parse_cfg_line(char *line, struct common_cfg *cfg);
+//parse a line of the peer info file
+struct peer_info parse_peer_line(char *line);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "init.h"
 
-#define CFG_PATH "config/Common.cfg"
+#define COMMON_CFG_PATH "config/Common.cfg"
+#define PEER_CFG_PATH "config/PeerInfo.cfg"
 
 int main(int argc, char *argv[]) {
     struct common_cfg cfg;
+    struct peer_info *peers = NULL;
+    int num_peers;
+    int i;
    
-    cfg = read_cfg(CFG_PATH);
+    cfg = read_cfg(COMMON_CFG_PATH);
 
     printf("NumberOfPreferredNeighbors = %d\n", cfg.n_preferred_neighbors);
     printf("UnchokingInterval = %d\n", cfg.unchoke_interval);
@@ -14,6 +18,18 @@ int main(int argc, char *argv[]) {
     printf("FileName = %s\n", cfg.file_name);
     printf("FileSize = %d\n", cfg.file_size);
     printf("PieceSize = %d\n", cfg.piece_size);
+
+    peers = read_peers(PEER_CFG_PATH, &num_peers);
+
+    for (i = 0 ; i < num_peers ; i++)
+    {
+        struct peer_info info = peers[i];
+
+        printf("peer_id: %d\n", info.peer_id);
+        printf("hostname: %s\n", info.hostname);
+        printf("port: %d\n", info.port);
+        printf("has_file: %d\n", info.has_file);
+    }
 
     return 0;
 }
