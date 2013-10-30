5cc3ceb65fce1cedb862115fa937fc6a688dd1e9
Ryan <murphyslaw480@gmail.com>
2013-10-30 11:26:22 -0400

    Add helper functions and fix compiler errors

---
+++ lib/reciever.h
@@ -0,0 +1,34 @@
+#ifndef _reciever_h
+#define _reciever_h
+
+#include "message.h"
+
+/* helper functions for sending messages
+ */
+
+//error codes
+#define NO_INTERESTING_PIECE -1
+#define INCORRECT_MSG_TYPE -2
+
+/* Try to parse data recieved from a message as a handshake
+ * If successful, return id of peer who sent handshake
+ * Else, return -1
+ */
+int parse_handshake_msg(char message[], int n_bytes);
+
+ /*
+  * Try to parse data recieved from a message as a normal message
+  * return 1 on success, 0 on failure
+  * place parsed message in mess
+ */
+int parse_normal_msg(char message[], int n_bytes, struct mess_normal *mess);
+
+
+/* Check a recieved BITFIELD message for an interesting piece.
+ * Return the index of the first interesting piece, or -1 if none is found.
+ * returns -2 if bitfield_msg.type != BITFIELD
+ */
+int find_interesting_piece(bitfield_t self_bits, struct mess_normal bitfield_msg);
+
+
+#endif
---
+++ lib/sender.h
@@ -0,0 +1,9 @@
+#ifndef _sender_h
+#define _sender_h
+
+#include "message.h"
+
+/* helper functions for sending messages
+ */
+
+#endif
