74f58c74fe8c438bd73bb99502e2cb0337528b76
Ryan <murphyslaw480@gmail.com>
2013-11-14 14:15:02 -0500

    function templates to read and write piece content.

--- lib/file_handler.h
+++ lib/file_handler.h
@@ -7,11 +7,20 @@ int file_split(char* FILENAME, int FS, int PS, int P_ID);
  * peer with ID P_ID
  */
 
-
-int file_merge(char* FILENAME, int FS, int PS, int P_ID){
+int file_merge(char* FILENAME, int FS, int PS, int P_ID);
 /* write a file with name FILENAME of size FS 
  * from pieces of size PS bytes saved under directory of
  * peer with ID P_ID
  */
+
+/* read the content from a local piece file into buffer
+ * return size of content read or a negative number on error
+ */
+long read_piece(unsigned int piece_idx, char buffer[]);
+
+/* get the content from a piece by reading the local piece file
+ * return amount written on success or negative number on error
+ */
+long write_piece(unsigned int piece_idx, char buffer[]);
 
 #endif
