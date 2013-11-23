f73f85c0fd2fd8d58b3cf8e6f39c5cbfc5304d76
Ryan <murphyslaw480@gmail.com>
2013-11-23 17:26:21 -0500

    support arbitrary-sized bitfield

--- lib/peer.h
+++ lib/peer.h
@@ -2,6 +2,7 @@
 #define _peer_h
 
 #include <time.h>
+#include <stdlib.h>
 #include "init.h"
 #include "receiver.h"
 
@@ -10,6 +11,11 @@ int peer_handle_data(struct peer_info *peer, message_t msg_type,
                      
 int peer_handle_periodic(struct peer_info *peer);
 
+//randomly choose a piece index owned by other peer and not us
+//return -1 if no interesting piece
 int find_interesting_piece(bitfield_t my_bitfield, bitfield_t other_bitfield);
+
+//returns a non-zero value if my_bitfield has a bit set for the piece idx
+int has_piece(int idx, bitfield_t my_bitfield);
 
 #endif
