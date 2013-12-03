6aa7a6892aa51f76c04da08a9729b842fa777ed3
Ryan <murphyslaw480@gmail.com>
2013-12-03 13:55:48 -0500

    save piece with own peer id

--- src/peer.c
+++ src/peer.c
@@ -101,8 +101,7 @@ int peer_handle_data(struct peer_info *peer, message_t msg_type,
         {
             // write the payload to disc
             unsigned int piece_idx = unpack_int(payload); // TODO: does this break?
-            //TODO: is this right? need to pass the id of peer that sent the piece
-            extract_and_save_piece(nbytes, payload, peer->peer_id);     
+            extract_and_save_piece(nbytes, payload, our_peer_id);     
             // update our_bitfield
             bitfield_set(our_bitfield, piece_idx);
             // increment pieces_this_interval field of peer_info
